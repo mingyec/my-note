9a795b846a84f7bee6bf1c7d9948258cea4ec34f
mingyec <mingyec@163.com>
2018-04-12 12:50:36 +0800

    加入svg-sprite；添加sass支持

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import material from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
 import router from './router'
+import './icons'
 
 Vue.config.productionTip = false
 Vue.use(material)
